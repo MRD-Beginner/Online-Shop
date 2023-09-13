7c48d7db5defbf13a338c24c3946d4b67b171638
Rizki Dalfi <muhammad.rizki3405@student.unri.ac.id>
2023-09-13 10:32:15 +0700

    feature : add index page

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <h2>Hallo</h2>
+
+    <?php
+        header('location: ../adminpanel');
+    ?>
+</body>
+</html>
